aa685cee2654ee81e95168062551c62fd1863fe7
Sammy Herring <sammy.herring@me.com>
2016-04-06 23:17:32 +0100

    Create boot_mark.py

---
+++ boot_mark.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import math
+import time
+import scrollphat
+import sys
+
+def millis():
+    return int(round(time.time() * 1000))
+
+scrollphat.set_brightness(2)
+scrollphat.write_string("Mark Lane  |||  SportTrax  |||  marklane.ml  |||  ") #Leave x number of blank spaces to prevent overlap
+
+while True:
+    try:
+        scrollphat.scroll()
+        time.sleep(0.1)
+    except KeyboardInterrupt:
+        scrollphat.clear()
+        sys.exit(-1)
